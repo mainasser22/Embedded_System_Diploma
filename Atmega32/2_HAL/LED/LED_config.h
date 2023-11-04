e1f898a7869f1a6bf1e5de7996cb4f1d3c1e8a2a
mai <mainasser2014@gmail.com>
2023-11-04 14:02:28 +0200

    Drivers

---
+++ Atmega32/2_HAL/LED/LED_config.h
@@ -0,0 +1,14 @@
+/*
+ * LED_config.h
+ *
+ *      Author: Mai Nasser
+ *      Layer:HAL
+ *      SWC:LED
+ */
+
+#ifndef HAL_LED_LED_CONFIG_H_
+#define HAL_LED_LED_CONFIG_H_
+
+
+
+#endif /* HAL_LED_LED_CONFIG_H_ */
